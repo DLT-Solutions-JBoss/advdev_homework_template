c10bb84db3cb7a26a1318827d8db03748519e28e
Rick Stewart <rick.stewart@dlt.com>
2019-04-29 09:57:33 -0400

    Shouldn't have to add params to Jenkins but...

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -15,10 +15,16 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # Set up Jenkins with sufficient resources
 oc new-app jenkins-persistent \
   -p ENABLE_OAUTH=true \
-  -p MEMORY_LIMIT=2Gi \
-  -p VOLUME_CAPACITY=4Gi\
+  -p MEMORY_LIMIT=4Gi \
+  -p VOLUME_CAPACITY=10Gi \
   -p DISABLE_ADMINISTRATIVE_MONITORS=true \
+  -env JENKINS_JAVA_OVERRIDES="-Dhudson.slaves.NodeProvisioner.initialDelay=0 -Dhudson.slaves.NodeProvisioner.MARGIN=50 \
+  -Dhudson.slaves.NodeProvisioner.MARGIN0=0.85 \
+  -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=300" \ 
   -n $GUID-jenkins
+
+# 
+oc set resources dc/jenkins --limits=memory=4Gi,cpu=4 --requests=memory=2Gi,cpu=2 -n $GUID-jenkins
 
 # Create custom agent container image with skopeo
 oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11 USER root RUN yum -y install skopeo && yum clean all USER 1001' \ 

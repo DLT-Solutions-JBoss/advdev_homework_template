1a55fa312806364fd385bc9286cdc6144176a71e
Rick Stewart <rick.stewart@dlt.com>
2019-04-29 12:33:04 -0400

    renaming slave pod

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -24,7 +24,7 @@ oc set resources dc/jenkins --limits=memory=4Gi,cpu=4 --requests=memory=2Gi,cpu=
 
 # Create custom agent container image with skopeo
 oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11 USER root RUN yum -y install skopeo && yum clean all USER 1001' \ 
- --name=skopeo-pod \
+ --name=jenkins-agent-appdev \
  -n $GUID-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
